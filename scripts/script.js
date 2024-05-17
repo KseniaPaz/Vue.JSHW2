a1c3f2c8d541a58fbbf1fab99341a75716f25a3a
KseniaPaz <knopik-ksu@mail.ru>
2024-05-17 10:45:52 +0500

    hw2 added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 new Vue({
-    el: '.blog__items',
+    el: '.article_items',
     data: {
         articles: [
             {
@@ -21,37 +21,19 @@ new Vue({
                 itemData: "25 December,2022"
             },
             {
-                image: "Image1.jpg",
+                image: "Image4.jpg",
                 imgText: "Kitchan Design",
                 itemTitle: "Let’s Get Solution For Building Construction Work",
                 itemData: "26 December,2022"
             },
             {
-                image: "Image2.jpg",
+                image: "Image5.jpg",
                 imgText: "Living Design",
                 itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
                 itemData: "22 December,2022"
             },
             {
-                image: "Image3.jpg",
-                imgText: "Interior Design",
-                itemTitle: "Best For Any Office & Business Interior Solution",
-                itemData: "25 December,2022"
-            },
-            {
-                image: "Image1.jpg",
-                imgText: "Kitchan Design",
-                itemTitle: "Let’s Get Solution For Building Construction Work",
-                itemData: "26 December,2022"
-            },
-            {
-                image: "Image2.jpg",
-                imgText: "Living Design",
-                itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
-                itemData: "22 December,2022"
-            },
-            {
-                image: "Image13.jpg",
+                image: "Image6.jpg",
                 imgText: "Interior Design",
                 itemTitle: "Best For Any Office & Business Interior Solution",
                 itemData: "25 December,2022"
